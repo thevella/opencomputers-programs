f69a0328f476ec3f7c271e20eaa9513bc43be04a
mitchell <thevella@users.noreply.github.com>
2020-02-27 00:26:54 -0500

    Documentation

--- infuser-share/lib/infuser-share-base.lua
+++ infuser-share/lib/infuser-share-base.lua
@@ -85,18 +85,26 @@ local function loadTransposers()
     end
 end
 
+-- Check if the item can be added to the inventory
 local inInventory(transposer, stackTest, inv)
     local stacks = transposer.obj.getAllStacks(inv)
-
     local stack = nil
 
-    local next = next
 
+    local next = next
+    -- Only loop over the stacks once, if not there
+    -- Fail
     repeat
+        -- Grab next stack
         stack = stacks()
 
+        -- If the stack exists
         if stack then
+            -- If the item is nil, it is empty and there
+            -- is space, otherwise check other conditions
             if next(stack) then
+                -- If the stack has the same name and the size is less
+                -- than the max stack size, there is space. Return true
                 if stack["name"] == stackTest["name"] and stack.size < stack.maxSize then
                     return true
                 end
@@ -104,10 +112,10 @@ local inInventory(transposer, stackTest, inv)
                 return true
             end
         end
-
         os.sleep(0)
     until (not stack)
 
+    -- If the stack wasn't in inventory, return false
     return false
 end
 
@@ -115,7 +123,6 @@ end
 local function hasExtraAndReg(transposer, extras, threshold)
     -- all of the stacks in the in inventory
     local stacks = transposer.obj.getAllStacks(transposer.in)
-
     -- The current stack, initialized in the loop
     local stack = nil
 
@@ -139,11 +146,6 @@ local function hasExtraAndReg(transposer, extras, threshold)
     -- While either hasReg or hasExtra is false
     -- Look for regular and extra items
     while (not slots.reg) or (not slots.extra) do
-        if tries > threshold then
-            slots.failed = true
-            break
-        end
-
         -- Next stack item
         stack = stacks()
         -- Increment the slot
@@ -181,13 +183,32 @@ local function hasExtraAndReg(transposer, extras, threshold)
                     slots.reg = slot
                 end
             end
+        -- If the stack was nil, we reached the end of the
+        -- stacks
         else
+            -- If tries is greater than set threshold,
+            -- break and return failed
+            if tries >= threshold then
+                slots.failed = true
+                break
+            end
+
+            -- Grab new stacks object
             stacks = transposer.obj.getAllStacks(transposer.in)
+
+            -- Reset slot to 0, but is incremented before next evaluation,
+            -- it is set to -1
             slot = -1
+
+            -- Reset slots to nil so we do not skip checking new
             slots.reg = nil
             slots.extra = nil
+
+            -- Reset sizes of largest stack to 0
             sizes.reg = 0
             sizes.extra = 0
+
+            -- increment tries
             tries = tries + 1
 
         end
@@ -195,39 +216,58 @@ local function hasExtraAndReg(transposer, extras, threshold)
         os.sleep(0)
     end
 
+    -- Return placements of the slots, and whether it failed
     return slots
 end
 
 
 function infuser_share_base.main(extras, debugChoice, threshold)
+    -- Grab debug from call
     debug = debugChoice
 
+    -- Whether to check for the existence of
+    -- new stacks
     local reSlot = true
 
+    -- Load all of the transposers and their
+    -- side values
     loadTransposers()
-    
+
+    local next = next
+
+    -- Quit if no transposer are found
+    if not next(transposers) then
+        print("\n\nFailure!")
+        print("  No transposers detected on network!\n\n")
+        os.exit()
+    end
+
     local slots = nil
+
+
     while true do
+        -- Loop as many as the threshold
         for try=1,threshold do
+            -- Loop through all transposers
             for i=1,#transposers do
+                -- Only reslot when needed
                 if reSlot then
                     slots = hasExtraAndReg(transposers[i], extras, threshold)
                     reSlot = false
                 end
 
-                if slots.failed then
-                    reslot = true
-
-                else
+                -- If it failed, reslot
+                if not slots.failed then
                     print("success")
+                else
+                    reslot = true
                 end
-
-
 
                 os.sleep(0)
             end
             os.sleep(0)
         end
+        -- Reslot if reached threshold
         reSlot = true
     end
 end
